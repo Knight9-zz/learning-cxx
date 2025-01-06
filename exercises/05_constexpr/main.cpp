37b9b07d0e92213c419bd904aa69a5a57ded376e
Knight9-zz <1073490419@qq.com>
2025-01-06 06:52:32 -0800

    test01

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,26 +1,34 @@
 #include "../exercise.h"
 
 constexpr unsigned long long fibonacci(int i) {
-    switch (i) {
-        case 0:
-            return 0;
-        case 1:
-            return 1;
-        default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+  switch (i) {
+  case 0:
+    return 0;
+  case 1:
+    return 1;
+  default:
+    // return fibonacci(i - 1) + fibonacci(i - 2);
+    unsigned long long a = 0;
+    unsigned long long b = 1;
+    for (int j = 2; j <= i; ++j) {
+      unsigned long long temp = b;
+      b = a + b;
+      a = temp;
     }
+    return b;
+  }
 }
 
 int main(int argc, char **argv) {
-    constexpr auto FIB20 = fibonacci(20);
-    ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
-    std::cout << "fibonacci(20) = " << FIB20 << std::endl;
+  constexpr auto FIB20 = fibonacci(20);
+  ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
+  std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
-    // TODO: 观察错误信息，修改一处，使代码编译运行
-    // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
-    constexpr auto ANS = fibonacci(ANS_N);
-    std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
+  // TODO: 观察错误信息，修改一处，使代码编译运行
+  // PS: 编译运行，但是不一定能算出结果……
+  constexpr auto ANS_N = 90;
+  constexpr auto ANS = fibonacci(ANS_N);
+  std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
-    return 0;
+  return 0;
 }
--- exercises/13_class/main.cpp
+++ exercises/13_class/main.cpp
@@ -9,27 +9,30 @@
 // 因此必须提供构造器来初始化字段。
 // READ: 构造器 <https://zh.cppreference.com/w/cpp/language/constructor>
 class Fibonacci {
-    size_t cache[16];
-    int cached;
+  size_t cache[16];
+  int cached;
 
 public:
-    // TODO: 实现构造器
-    // Fibonacci()
+  // TODO: 实现构造器
+  Fibonacci() {
+    this->cache[0] = 0, this->cache[1] = 1;
+    this->cached = 2;
+  }
 
-    // TODO: 实现正确的缓存优化斐波那契计算
-    size_t get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
-        }
-        return cache[i];
+  // TODO: 实现正确的缓存优化斐波那契计算
+  size_t get(int i) {
+    for (; cached <= i; ++cached) {
+      cache[cached] = cache[cached - 1] + cache[cached - 2];
     }
+    return cache[i];
+  }
 };
 
 int main(int argc, char **argv) {
-    // 现在类型拥有无参构造器，声明时会直接调用。
-    // 这个写法不再是未定义行为了。
-    Fibonacci fib;
-    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
-    std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
-    return 0;
+  // 现在类型拥有无参构造器，声明时会直接调用。
+  // 这个写法不再是未定义行为了。
+  Fibonacci fib;
+  ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
+  std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
+  return 0;
 }
